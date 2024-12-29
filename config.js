7709016707332cdd4c3cddaec6bf62fa1081aa55
shaMika12-bit <shanushamika2@gmail.com>
2024-12-29 10:17:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
-MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
+SESSION_ID: process.env.SESSION_ID || "PqARzTBD#byxjsUv7F0qRuQrbM8iSYIoU5a7LN2AW4pe9T0ITEeE", // Add your session id
+MONGODB: process.env.MONGODB || "mongodb://mongo:ATUdNXYlCCXxOvdqUlXjWObNPwdkdpib@autorack.proxy.rlwy.net:42669", // Add your mongodb url
 };
